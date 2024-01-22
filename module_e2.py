33ace15ddb6a515e79d00a95ef55228fe5b8d714
Jaron Johnson <jarontjohnson@yahoo.com>
2024-01-22 15:18:25 -0800

    Day3HW

---
+++ module_e2.py
@@ -0,0 +1,10 @@
+def calculate_square_footage(length, width):
+    "calculate the square footage of a house."
+    square_footage = length * width
+    return square_footage
+
+def calculate_circumference(radious):
+    "calculate the circumference of a circle"
+    import math
+    circumference = 2 * math.pi * radious
+    return circumference
